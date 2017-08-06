54dd11bef6a4f80bbda46a8e16df0fd64b94f6e0
lahatme <30632811+lahatme@users.noreply.github.com>
2017-08-06 13:15:53 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import Game from './Game.js';
 import {Button} from 'react-mdl';
-
+import SweetAlert from 'sweetalert-react';
+import 'sweetalert/dist/sweetalert.css';
 class App extends Component {
 
     constructor() {
@@ -16,30 +17,63 @@ class App extends Component {
             txtMines: 9,
             startNewGame: false,
             maxMines: 8*10,
+            showAlert: false,
+            alertTitle: "Allowed up to 300!",
+            alertType: 'warning',
+            alertConfirmButtonText: 'Back'
         };
     }
     //When the width field changes, we change the width of the matrix and limit the number of mines to the number of cells in the new matrix
 
     set_Board_Width(width) {
-        if (width < 1) this.width = 1;
-        if (width > 300) this.width = 300;
-        let updateMaxMines = width * this.state.height;
-        if (this.state.mines > updateMaxMines) this.state.mines=updateMaxMines;
-        this.setState({width:width, txtWidth: width, txtMines:this.props.mines , maxMines:updateMaxMines, startNewGame: false});
+        if (width < 1) width = 1;
+        if (width > 300) {
+            width = 300;
+            this.setState({
+                showAlert: true,
+                alertTitle: "Allowed up to 300!",
+                alertType: 'warning',
+                alertConfirmButtonText: 'Back'
+                });
+        }
+        this.set_Board_Mines(this.state.mines,this.state.height,width);
+
+        
+        this.setState({width:width, txtWidth: width, txtMines:this.state.txtMines ,  startNewGame: false});
+
+
     }
     //When the height field changes, we change the width of the matrix and limit the number of mines to the number of cells in the new matrix
 
     set_Board_Height(height) {
-        if (height < 1) {this.height = 1;}
-        if (height > 300) {this.height = 300;}
-        let updateMaxMines = height * this.state.width;
-        this.setState({ height:height, txtHeight: height, txtMines: this.props.mines,maxMines: updateMaxMines, startNewGame: false});
+        if (height < 1) height = 1;
+        if (height > 300) {
+            height = 300;
+            this.setState({
+                showAlert: true,
+                alertTitle: "Allowed up to 300!",
+                alertType: 'warning',
+                alertConfirmButtonText: 'Back'
+                });
+        }      
+
+        this.set_Board_Mines(this.state.mines,height,this.state.width);
+
+        this.setState({ height:height, txtHeight: height, txtMines: this.state.txtMines, startNewGame: false});
+
     }
     //Check that the number of mines is greater than 1 and smaller / equal to the size of the matrix and update the number of mines
 
-    set_Board_Mines(mines) {
+    set_Board_Mines(mines,height,width) {
         if (mines < 1) {this.mines = 1;}
-        if (mines > this.state.maxMines) {mines = this.state.maxMines;}
+        if (mines >height*width) {
+            mines =height*width;
+                this.setState({
+                showAlert: true,
+                alertTitle: "Allowed up to " +  height*width + " mines",
+                alertType: 'warning',
+                alertConfirmButtonText: 'Back'
+                });}
         this.setState({mines: mines, txtMines : mines, startNewGame: false});
     }
     //If the button is checked then you will expose the board
@@ -88,7 +122,7 @@ class App extends Component {
                         </td>
                         <td>
                             Mines:
-                            <input onInput={(e) => this.set_Board_Mines(e.target.value)}
+                            <input onInput={(e) => this.set_Board_Mines(e.target.value,this.state.txtHeight,this.state.txtWidth)}
                                     defaultValue={this.state.txtMines} />
                         </td>
                         <td>
@@ -102,6 +136,13 @@ class App extends Component {
                         boardHeight={this.state.height} boardWidth={this.state.width} mines={this.state.mines}
                         superman={this.state.superman} startNewGame={this.state.startNewGame}/>
                 </div>
+                <SweetAlert
+                    show={this.state.showAlert}
+                    title={this.state.alertTitle}
+                    type={this.state.alertType}
+                    confirmButtonText={this.state.alertConfirmButtonText}
+                    onConfirm={() => this.setState({showAlert: false})}
+                />
             </div>
         );
     }
